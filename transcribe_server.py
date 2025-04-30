be55c57f94b3195eb99a5d780ce02610cd8faddf
Yehao Liu <yehao@google.com>
2025-04-30 15:38:15 +0800

    some changes

--- transcribe_server.py
+++ transcribe_server.py
@@ -234,6 +234,11 @@ class TranscriptionServer:
         if not current_all_segments and not has_new_speech:
             logger.debug("No new speech segments detected by VAD.")
             return None
+        
+        debug_audio_dir = "debug_audio_websites"
+        if not os.path.exists(debug_audio_dir):
+            os.makedirs(debug_audio_dir)
+        timestamp_str = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
         filename = os.path.join(debug_audio_dir, f"to_website_{timestamp_str}_s{current_start_index}_e{current_end_index}.wav")
         self.save_tensor_to_wav(self.all_chunks, 16000, filename) # Debug line
 
